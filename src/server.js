cacedf8a3a818dba3b397c714ed7c61b2a55df29
ralph <ralph.souza@itmss.com.br>
2020-07-05 17:50:24 -0300

    Refatoração de código com Nunjucks através da criação do diretório do partials.

--- src/server.js
+++ src/server.js
@@ -4,10 +4,21 @@ const server = express()
 // Configuração da pasta pública
 server.use(express.static("public"))
 
+// Configurando a template engine 'NUNJUCKS'
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 // Configuração de caminhos
 // Página home
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.render("create-point.html")
 })
 
 // Ligar o servidor
